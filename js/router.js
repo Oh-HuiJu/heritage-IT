178c1e43ac1b74c9dfb271921ded5f7bc33bde97
오희주 <ohj4875@gmail.com>
2025-08-20 17:48:40 +0900

    미디어쿼리 80% 완료

--- js/router.js
+++ js/router.js
@@ -1,5 +1,5 @@
 const routes = {
-  "/":                 "/html/home.html",
+  "/":                 "/html/company-history.html",
   "/product-over":     "/html/product-overview.html",
   "/product-how":      "/html/product-how.html",
   "/platform":         "/html/platform.html",
